5c17b16ad703e8e766ab488ed3dd5cbfe93faa46
sizaif <sizaif2000@gmail.com>
2021-07-01 22:24:01 +0800

    delete setup_cryptopp.sh

--- setup_cryptopp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/weidai11/cryptopp/archive/CRYPTOPP_5_6_5.tar.gz \
-&& tar -xzf CRYPTOPP_5_6_5.tar.gz \
-&& mv cryptopp-CRYPTOPP_5_6_5 cryptopp \
-&& cd cryptopp \
-&& make
-
